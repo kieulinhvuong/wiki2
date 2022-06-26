08bc538b44abf80a138bf7a6586582cbbe11b5f2
kieulinhvuong <kieulinh.jan22@gmail.com>
2022-06-26 01:46:31 -0500

    completed entrypage and indexpage

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:entry>", views.subPage, name="entrypage"),
+    path("wiki/error", views.subPage, name="errorpage")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,15 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def subPage(request, entry):
+    content = util.get_entry(entry)
+    if (content):
+        return render(request, "encyclopedia/entrypage.html", {
+        "entries": util.get_entry(entry)
+    })
+    else:
+        return render(request, "encyclopedia/errorpage.html")
+
+
+
+
